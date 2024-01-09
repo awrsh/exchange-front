12bed273940511e985e3981855d9f63d689fcf70
awrsh <arashzarandi@gmail.com>
2024-01-09 16:02:28 +0330

    fix name trusttether

--- src/componets/invite-friends/InviteInformation.tsx
+++ src/componets/invite-friends/InviteInformation.tsx
@@ -4,7 +4,7 @@ const InviteInformation = () => {
     return (
         <div className="bg-white flex justify-between flex-col rounded-xl p-3">
             <div className="border-b border-int pb-5">
-                <p className="font-bold text-xl">کسب درآمد میلیونی با دعوت دوستان به تراست تتر</p>
+                <p className="font-bold text-xl">کسب درآمد میلیونی با دعوت دوستان به  بیت آسیا</p>
                 <span className="font-regular text-[13px] text-neutral-500 text-center w-full block mt-4">کد دعوت و یا لینک زیر را برای دوستان خود ارسال کنید و با تایید هویت تعداد هرچه بیشتر دوستانتان، مادام‌العمر تا 55درصد پاداش دریافت نمایید</span>
             </div>
             <div className="space-y-4">
@@ -15,7 +15,7 @@ const InviteInformation = () => {
                 </div>
                 <div className="flex items-center w-full text-neutral-400 justify-between bg-neutral-100 p-3 rounded-lg">
                     <p className="font-bold text-[13px]">لینک دعوت</p>
-                    <p className="text-int font-num text-[10px] lg:text-[14px]">trusttether.com/register?ref=LOJFFD</p>
+                    <p className="text-int font-num text-[10px] lg:text-[14px]">bitasia.com/register?ref=LOJFFD</p>
                     <IoCopy />
                 </div>
             </div>
