9c7cb8d093657261986dbcc40a4b7b53be781cd7
Abbas Ebadian <abbasebadiann@gmail.com>
2024-04-12 15:36:22 +0330

    Update action.ts

--- src/services/action.ts
+++ src/services/action.ts
@@ -5,10 +5,10 @@ import client from "./utils/client";
 
 
 export const createRialPayment = async (data:{card_number:string,amount:number}) => {
-    const url = getRoute({ route: "/idp/create_rial_payment/" });
+    const url = getRoute({ route: "/transactions/rial/deposit/new/" });
     return await client<{url:string}>({ url,method:"POST",data });
 };
 export const withdreawPayment = async (data:{card_number:string,amount:number}) => {
-    const url = getRoute({ route: "/idp/rial_withdraw/" });
+    const url = getRoute({ route: "/transactions/rial/withdraw/" });
     return await client<{url:string}>({ url,method:"POST",data });
 };
